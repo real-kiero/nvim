8956ba81d3f41ecf8c2f8bccbe8332f7f5487f94
Kiero <38412336+real-kiero@users.noreply.github.com>
2025-10-10 18:06:19 +0100

    Updated telescope
    Added telescope file browser to replace nvim_tree. As mentioned before I
    wasn't properly using nvim-tree which I removed. Because I am now using
    nvim in a work capacity and actively trying to contribute to open-source
    projects (to varying success), I do find it useful to have a file
    browser at some capacity

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,13 +2,29 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     cmd = "Telescope",
-    keys = {
-      { "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "find files" },
-      { "<leader>fg", "<cmd>Telescope live_grep<CR>", desc = "live grep" },
-      { "<leader>fr", "<cmd>Telescope oldfiles<CR>", desc = "recent files" },
-      { "<leader>fh", "<cmd>Telescope help_tags<CR>", desc = "help tags" },
-    },
     dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          prompt_prefix = " ",
+          selection_caret = " ",
+          path_display = { "truncate" },
+        },
+        extensions = {
+          file_browser = {
+            theme = "ivy",
+            hijack_netrw = true,
+          },
+        },
+      })
+    end,
+  },
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").load_extension("file_browser")
+    end,
   },
   {
     "nvim-telescope/telescope-ui-select.nvim",
