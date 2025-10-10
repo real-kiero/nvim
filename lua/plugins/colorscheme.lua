ff96070a8385849eb04b58061c139ccd5ed11ef8
Kiero <38412336+real-kiero@users.noreply.github.com>
2025-10-10 18:02:08 +0100

    colorscheme change
    I keep flip-flopping on colorschemes, because I'm using a series of
    different monitors it's somewhat difficult to find one I totally gel
    with, I may at some point introduce a theme switcher with my favourite
    colorschemes installed and allow for a "little" bloat

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,31 +1,11 @@
 return {
   {
-    "rebelot/kanagawa.nvim",
+    "olivercederborg/poimandres.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-      require('kanagawa').setup({
-        compile = false,
-        undercurl = true,
-        commentStyle = { italic = true },
-        functionStyle = {},
-        keywordStyle = { italic = true},
-        statementStyle = { bold = true },
-        typeStyle = {},
-        transparent = false,
-        dimInactive = false,
-        terminalColors = true,
-        colors = {
-          palette = {},
-          theme = { wave = {}, lotus = {}, dragon = {}, all = {} }
-        },
-        theme = "wave",
-        background = {
-          dark = "wave",
-          light = "lotus"
-        },
-      })
-      vim.cmd.colorscheme("kanagawa")
+      require('poimandres').setup({})
+      vim.cmd.colorscheme("poimandres")
     end,
   },
 }
