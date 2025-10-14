4e2a2c3f3ad8295a59843aaeb64da5852adcb5ce
Kiero <38412336+real-kiero@users.noreply.github.com>
2025-10-14 16:23:50 +0100

    Added snacks.nvim for nicer notifications and file loading
    Utalising Folke's snacks.nvim added notifier, bigfile and quickfile with
    default config

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      notifier = { enabled = true },
+      bigfile = { enabled = true },
+      quickfile = { enabled = true },
+    },
+  },
+}
